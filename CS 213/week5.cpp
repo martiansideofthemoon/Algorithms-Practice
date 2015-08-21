967777e5329cf3925bcf6b54c315e7286aeb4962
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-08-22 02:59:18 +0530

    basic merge sort with counting complete

--- CS 213/week5.cpp
+++ CS 213/week5.cpp
@@ -9,6 +9,48 @@ vector<int> p3;
 vector<int> inv_p1;
 vector<int> inv_p2;
 vector<int> inv_p3;
+list<int> merge_with_count(list<int> l1,list<int>::iterator l1_begin,list<int>::iterator l1_end,
+							list<int>::iterator l2_begin,list<int>::iterator l2_end,
+							int size1,int size2,int &count)
+{
+	while (l1_begin!=l1_end && l2_begin!=l2_end)
+	{
+		if (*l1_begin<*l2_begin)
+		{
+			l1_begin++;
+			size1--;
+		}
+		else
+		{
+			l1.insert(l1_begin,*l2_begin);
+			l2_begin = l1.erase(l2_begin);
+			size2--;
+			count+=size1;
+		}
+	}
+	if (l1_begin==l1_end)
+	{
+		while (l2_begin!=l2_end)
+		{
+			l1.insert(l1_begin,*l2_begin);
+			l2_begin = l1.erase(l2_begin);
+			size2--;
+			count+=size1;
+		}
+	}
+}
+void merge_sort(list<int> &l1,list<int>::iterator l1_begin,list<int>::iterator l1_end,int size,int &count)
+{
+	if (l1_begin!=l1_end)
+	{
+		list<int>::iterator midway=l1_begin;
+		for (int i=0;i<size/2;i++)
+			midway++;
+		merge_sort(l1,l1_begin,midway,size/2,count);
+		merge_sort(l1,midway,l1_end,size-size/2,count);
+		merge_with_count(l1,l1_begin,midway,midway,l1_end,size/2,size-size/2,count);
+	}
+}
 bool check_lesser(int i,int j)
 {
 	int tests_passed = 0;
@@ -59,7 +101,7 @@ int main()
 	for (int i=0;i<n;i++)
 		inv_p3[p3[i]]=i;
 
-	sort(p4.begin(),p4.end(),check_lesser);
+	//sort(p4.begin(),p4.end(),check_lesser);
 
-	
+
 }
