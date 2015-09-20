cf34916360d043c2f69d510561c89c4af2e3c29d
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-09-20 22:45:41 +0530

    week7 done

--- CS 213/week7.cpp
+++ CS 213/week7.cpp
@@ -12,6 +12,7 @@ struct ltstr
 };
 void print_set(map<int, set<const char*, ltstr> >::iterator i1)
 {
+	cout << i1->first << " ";
 	set<const char*, ltstr>::iterator set_iter = (i1->second).begin();
 	set<const char*, ltstr>::iterator set_iter_end = (i1->second).end();
 	while(set_iter!=set_iter_end)
@@ -33,17 +34,26 @@ int main()
 		cin >> operation;
 		if (operation=='O')
 		{
-			char * name;
+			char * name = new char[80];
 			cin >> name;
+			if (accounts.find(name)!=accounts.end())
+			{
+				illegal_count++;
+				continue;
+			}
 			accounts[name]=0;
 			balances[0].insert(name);
 		}
 		else if (operation=='D')
 		{
-			char *name;
-			cin >> name;
+			char * name = new char[80];
 			int amount;
-			cin >> amount;
+			cin >> name >> amount;
+			if (accounts.find(name)==accounts.end())
+			{
+				illegal_count++;
+				continue;
+			}
 			int original_amount = accounts[name];
 			accounts[name]+=amount;
 			balances[original_amount].erase(name);
@@ -51,10 +61,15 @@ int main()
 		}
 		else if (operation=='W')
 		{
-			char *name;
+			char * name = new char[80];
 			cin >> name;
 			int amount;
 			cin >> amount;
+			if (accounts.find(name)==accounts.end() || amount>accounts[name])
+			{
+				illegal_count++;
+				continue;
+			}
 			int original_amount = accounts[name];
 			accounts[name]-=amount;
 			balances[original_amount].erase(name);
@@ -62,8 +77,13 @@ int main()
 		}
 		else if (operation=='C')
 		{
-			char *name;
+			char *name = new char[80];
 			cin >> name;
+			if (accounts.find(name)==accounts.end() || accounts[name]!=0)
+			{
+				illegal_count++;
+				continue;
+			}
 			int original_amount=accounts[name];
 			accounts.erase(name);
 			balances[original_amount].erase(name);			
@@ -74,13 +94,14 @@ int main()
 			cin >> start >> end;
 			map<int, set<const char*, ltstr> >::iterator i1 = balances.lower_bound(start);
 			map<int, set<const char*, ltstr> >::iterator i2 = balances.upper_bound(end);
-			i1++;
 			i2--;
 			while (i1!=i2)
 			{
+				if ((i1->second).size()!=0)
 				print_set(i1);
 				i1++;
 			}
+			if ((i1->second).size()!=0)
 			print_set(i1);
 		}
 		else if (operation=='E')
