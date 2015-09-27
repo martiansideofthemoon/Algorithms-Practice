8077d40aa622a25c59cdb660d1003062f59423ad
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-09-27 20:18:16 +0530

    Original question coded

--- CS 213/week8.cpp
+++ CS 213/week8.cpp
@@ -9,13 +9,13 @@ struct lane {
 	char direction;
 	int position;
 	int num_ships;
-	vector<vector<double> > ships;
-	double forbidden1=0;
-	double forbidden2=0;
-	double offset=0;
+	vector<vector<int> > ships;
 	vector<int> forbidden_time(int ship_no) 
 	{
 		vector<int> ship = ships[ship_no];
+		int forbidden1=0;
+		int forbidden2=0;
+		int offset=0;
 		if (direction=='E')
 		{
 			forbidden1=-1*ship[1]/u_ships;
@@ -32,9 +32,9 @@ struct lane {
 			forbidden1+=offset;
 			forbidden2+=offset;
 		}
-		vector<double> result;
-		result.pushback(forbidden1);
-		result.pushback(forbidden2);
+		vector<int> result;
+		result.push_back(forbidden1);
+		result.push_back(forbidden2);
 		return result;
 	}
 };
@@ -53,10 +53,52 @@ int main()
 			vector <int> v1;
 			int len,pos;
 			cin >> len >> pos;
-			v1.pushback(len);
-			v1.pushback(pos);
-			l1.ships.pushback(v1);
-			lanes.pushback(l1);
+			v1.push_back(len);
+			v1.push_back(pos);
+			l1.ships.push_back(v1);
+			lanes.push_back(l1);
 		}
+	}
+	vector<lane>::iterator iter = lanes.begin();
+	//map<int, vector<int> > intervals;
+	map<int, int> intervals;
+	while (iter!=lanes.end())
+	{
+		int num_ships=(*iter).num_ships;
+		for (int i=0;i<num_ships;i++)
+		{
+			vector<int> forbidden = (*iter).forbidden_time(i);
+			if ((forbidden[0]<t1 && forbidden[1]<t1) || (forbidden[0]>t2 && forbidden[1]>t2))
+				continue;
+			//intervals[forbidden[0]] = forbidden;
+			intervals[forbidden[0]]=0;
+			intervals[forbidden[1]]=1;
+		}
+		iter++;
+	}
+	map<int, int>::iterator i1 = intervals.begin();
+	int start=0,end=0;
+	int longest=0;
+	int forbid_ships=0;
+	bool isValidRegion=false;
+	while (i1!=intervals.end())
+	{	
+		if (isValidRegion==true)
+		{
+			end=i1->first;
+			isValidRegion=false;
+			if (end-start>longest)
+				longest=end-start;
+		}
+		if (i1->second==0)
+			forbid_ships++;
+		else
+			forbid_ships--;
+		if (forbid_ships==0)
+		{
+			start=i1->first;
+			isValidRegion=true;
+		}
+		i1++;
 	}
 }
