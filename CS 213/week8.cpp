a1db2f2e95cc2dbbe2a03ebf21a7dc9390d15189
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-09-28 00:59:41 +0530

    basic week8 working

--- CS 213/week8.cpp
+++ CS 213/week8.cpp
@@ -10,29 +10,30 @@ struct lane {
 	int position;
 	int num_ships;
 	vector<vector<int> > ships;
-	vector<int> forbidden_time(int ship_no) 
+	vector<double> forbidden_time(int ship_no) 
 	{
 		vector<int> ship = ships[ship_no];
-		int forbidden1=0;
-		int forbidden2=0;
-		int offset=0;
+		double crossing_offset=(double)width/u_ferry;
+		double forbidden1=0;
+		double forbidden2=0;
+		double offset=0;
 		if (direction=='E')
 		{
-			forbidden1=-1*ship[1]/u_ships;
-			forbidden2=forbidden1+ship[0]/u_ships;
-			offset=position*width/u_ferry;
-			forbidden1+=offset;
-			forbidden2+=offset;
+			forbidden1=-1.0*ship[1]/u_ships;
+			forbidden2=forbidden1+(double)ship[0]/u_ships;
+			offset=(double)position*width/u_ferry;
+			forbidden1=forbidden1-offset-crossing_offset;
+			forbidden2=forbidden2-offset;
 		}
 		else if (direction=='W')
 		{
-			forbidden1=ship[1]/u_ships;
-			forbidden2=forbidden1+ship[0]/u_ships;
-			offset=position*width/u_ferry;
-			forbidden1+=offset;
-			forbidden2+=offset;
+			forbidden1=(double)ship[1]/u_ships;
+			forbidden2=forbidden1+(double)ship[0]/u_ships;
+			offset=(double)position*width/u_ferry;
+			forbidden1=forbidden1-offset-crossing_offset;
+			forbidden2=forbidden2-offset;
 		}
-		vector<int> result;
+		vector<double> result;
 		result.push_back(forbidden1);
 		result.push_back(forbidden2);
 		return result;
@@ -56,31 +57,38 @@ int main()
 			v1.push_back(len);
 			v1.push_back(pos);
 			l1.ships.push_back(v1);
-			lanes.push_back(l1);
 		}
+		lanes.push_back(l1);
 	}
 	vector<lane>::iterator iter = lanes.begin();
 	//map<int, vector<int> > intervals;
-	map<int, int> intervals;
+	map<double, int> intervals;
 	while (iter!=lanes.end())
 	{
 		int num_ships=(*iter).num_ships;
 		for (int i=0;i<num_ships;i++)
 		{
-			vector<int> forbidden = (*iter).forbidden_time(i);
+			vector<double> forbidden = (*iter).forbidden_time(i);
 			if ((forbidden[0]<t1 && forbidden[1]<t1) || (forbidden[0]>t2 && forbidden[1]>t2))
 				continue;
 			//intervals[forbidden[0]] = forbidden;
-			intervals[forbidden[0]]=0;
-			intervals[forbidden[1]]=1;
+			intervals[forbidden[0]]+=1;
+			intervals[forbidden[1]]+=-1;
 		}
 		iter++;
 	}
-	map<int, int>::iterator i1 = intervals.begin();
-	int start=0,end=0;
-	int longest=0;
+	map<double, int>::iterator i1 = intervals.begin();
+	double start=0,end=0;
+	double longest=0.000;
 	int forbid_ships=0;
 	bool isValidRegion=false;
+	if (intervals.size()==0)
+	{
+		cout << t2-t1 << endl;
+		return 0;
+	}
+	if ((i1->first)>t1)
+		longest=(i1->first)-t1;
 	while (i1!=intervals.end())
 	{	
 		if (isValidRegion==true)
@@ -90,10 +98,7 @@ int main()
 			if (end-start>longest)
 				longest=end-start;
 		}
-		if (i1->second==0)
-			forbid_ships++;
-		else
-			forbid_ships--;
+		forbid_ships+=i1->second;
 		if (forbid_ships==0)
 		{
 			start=i1->first;
@@ -101,4 +106,8 @@ int main()
 		}
 		i1++;
 	}
+	i1--;
+	if ((i1->first)<t2 && (t2-i1->first)>longest)
+		longest=t2-i1->first;
+	cout << longest << endl;
 }
