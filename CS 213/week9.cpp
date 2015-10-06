41ed6d0095eb615f6a1c4e488fe2f3ef26359b4c
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-10-06 21:50:14 +0530

    week9 started

---
+++ CS 213/week9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class boolfunc {
+public:
+	boolfunc() {
+
+	}
+	void read() {
+		int terms;
+		cin >> terms;
+		for (int i = 0; i < terms; ++i)
+		{
+			int literals;
+			cin >> literals;
+			for (int j = 0; j < terms; ++j)
+			{
+				int literal;
+				cin >> literal;
+			}
+		}
+	}
+private:
+	vector<int> tree;
+
+};
+int main()
+{
+
+}
